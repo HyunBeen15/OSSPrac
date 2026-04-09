afa3c7bf714ccaa40afc8e32c07598a085321300
HyunBeen15 <chbn0105@gmail.com>
2026-04-09 14:46:32 +0900

    2026-04-09 개인과제6 ossprac2 수정

--- subject5/2.cpp
+++ subject5/2.cpp
@@ -1,20 +1,29 @@
 #include <iostream>
 using namespace std;
 
-int main() 
-{
+int main() {
     string name;
     string studentID;
+    string department;
+    string school;
 
-    cout << "이름을 입력하세요: ";
-    cin >> name;
+    cout << "이름 입력: ";
+    getline(cin, name);
 
-    cout << "학번을 입력하세요: ";
-    cin >> studentID;
+    cout << "학번 입력: ";
+    getline(cin, studentID);
+
+    cout << "학과 입력: ";
+    getline(cin, department);
+
+    cout << "학교 입력: ";
+    getline(cin, school);
 
     cout << "\n[출력 결과]" << endl;
     cout << "이름: " << name << endl;
     cout << "학번: " << studentID << endl;
+    cout << "학과: " << department << endl;
+    cout << "학교: " << school << endl;
 
     return 0;
 }
